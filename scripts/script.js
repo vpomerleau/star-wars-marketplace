ff50efc32ce0f0eaa2d402596a0540b45e4f54d6
Valerie Pomerleau <v.pomerleau@gmail.com>
2022-06-20 18:13:37 -0700

    style header, hero, footer

--- scripts/script.js
+++ scripts/script.js
@@ -8,25 +8,25 @@
 // "crew": "30-165"
 // "hyperdrive_rating": "2.0"
 
-const starshipsUrl = "https://swapi.dev/api/starships/"
-const vehiclesUrl = "https://swapi.dev/api/vehicles/"
+// const starshipsUrl = "https://swapi.dev/api/starships/"
+// const vehiclesUrl = "https://swapi.dev/api/vehicles/"
 
-let starshipsArray = [];
-let vehiclesArray = [];
+// let starshipsArray = [];
+// let vehiclesArray = [];
 
-axios.get(starshipsUrl)
-.then(response => {
-    // console.log(response.data.results);
-    response.data.results.forEach(object =>
-        starshipsArray.push(object));
-    console.log(starshipsArray);
-})
+// axios.get(starshipsUrl)
+// .then(response => {
+//     // console.log(response.data.results);
+//     response.data.results.forEach(object =>
+//         starshipsArray.push(object));
+//     console.log(starshipsArray);
+// })
 
-axios.get(vehiclesUrl)
-.then(response => {
-    // console.log(response.data.results);
-    response.data.results.forEach(object =>
-        vehiclesArray.push(object));
-    console.log(vehiclesArray);
-})
+// axios.get(vehiclesUrl)
+// .then(response => {
+//     // console.log(response.data.results);
+//     response.data.results.forEach(object =>
+//         vehiclesArray.push(object));
+//     console.log(vehiclesArray);
+// })
 
